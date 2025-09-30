f21b56547c578268fe60bc7199048ac006e97fbc
kyomi3263-Hue <k.youmi3263@gmail.com>
2025-09-30 18:39:45 +0900

    불필요한 코드 삭제 params: {} 추가

--- productService.js
+++ productService.js
@@ -13,10 +13,14 @@ deleteProduct()
 */
 //==========겟 프로덕트 리스트=======
 export async function getProductList(page, pageSize, keyword) {
-  const q = `?page=${page}&pasgeSize=${pageSize}&keyword=${keyword}`;
-  const finURL = `${baseURL}/products/${q}`;
   try {
-    const response = await axios.get(`${baseURL}/products`);
+    const response = await axios.get(`${baseURL}/products`, {
+      params: {
+        page,
+        pageSize,
+        keyword,
+      },
+    });
     console.log(`성공!!! : ${response.data}`);
   } catch (error) {
     console.error(`실패!!! : ${error.message}`);
