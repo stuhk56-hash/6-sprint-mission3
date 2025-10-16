f0fad6c93f5ebd13efe20dc42038d47d77efefe5
kimjs <kimjs910522@gmail.com>
2025-10-16 10:36:23 +0900

    sprint misstion 2 진행 내역

---
+++ class/ElectronicProduct.js
@@ -0,0 +1,9 @@
+import Product from "./Product.js";
+
+export default class ElectronicProduct extends Product{
+    constructor(name, description, price, tags, images, favoriteCount, manufacturer){
+        super(name, description, price, tags, images, favoriteCount);
+        this.manufacturer = manufacturer;
+    }
+}
+
