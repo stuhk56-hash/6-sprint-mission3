ad6ece6e8fd35d4e4f4967828366647d3d51837d
hyeonjun <stuhk56@gmail.com>
2026-01-27 16:21:15 +0900

    [정현준]sprint8

---
+++ jest-framework/src/controllers/notification-controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { create, object, coerce, number, string } from "superstruct";
+import * as notificationService from "../services/notification-service.js";
+import { AuthRequest } from "../middlewares/authenticate.js";
+
+const IdParamsStruct = object({
+  id: coerce(number(), string(), (value) => parseInt(value, 10)),
+});
+
+export async function readNotification(req: Request, res: Response) {
+  const { id } = create(req.params, IdParamsStruct);
+  await notificationService.readNotificationById(
+    id,
+    (req as AuthRequest).user?.id
+  );
+  res.status(204).send();
+}
+
