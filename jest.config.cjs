a20380e04a2cc42013c7708248969de86cd86483
hyeonjun <stuhk56@gmail.com>
2026-02-03 13:47:19 +0900

    sprint9-정현준

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
   moduleDirectories: ['node_modules', 'src'],
-  extensionsToTreatAsEsm: ['.ts'],
   testMatch: [
     '<rootDir>/tests/**/*.test.ts',
   ],
@@ -21,9 +23,10 @@ module.exports = {
   coverageDirectory: './coverage',
   coverageReporters: ['text', 'lcov'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+    '^.+\\.ts$': ['ts-jest', { tsconfig: { module: 'NodeNext' } }],
   },
   moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: '<rootDir>/' }),
     '\\.(css|less)$': 'identity-obj-proxy',
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
