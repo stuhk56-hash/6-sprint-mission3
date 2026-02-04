6488055b84b313c80feb94830a3cbbfab7914d27
hyeonjun <stuhk56@gmail.com>
2026-02-04 18:30:05 +0900

    sprint9-정현준

--- tests/test1.ts
+++ tests/test1.ts
@@ -2,9 +2,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
-  // options
-});
+const io = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
   // ...
